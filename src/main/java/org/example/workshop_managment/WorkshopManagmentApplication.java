6a600a18f0af2fbef134fd06dea588cd2b8c63ad
Armanpreet <armanpreetk90@gmail.com>
2025-09-04 14:26:48 +0530

    initial commit

---
+++ src/main/java/org/example/workshop_managment/WorkshopManagmentApplication.java
@@ -0,0 +1,13 @@
+package org.example.workshop_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopManagmentApplication.class, args);
+    }
+
+}
