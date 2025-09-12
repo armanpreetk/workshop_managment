36b5da6efbf01aed1564aabdd3549f74cd1572fa
bhumikachopra11 <bhumikachopra11@gmail.com>
2025-09-12 23:43:32 +0530

    Business services and repo commits

--- src/main/java/org/example/workshop_managment/repository/ManagerRepository.java
+++ src/main/java/org/example/workshop_managment/repository/ManagerRepository.java
@@ -2,5 +2,6 @@ package org.example.workshop_managment.repository;
 
 import org.example.workshop_managment.model.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface ManagerRepository extends JpaRepository<Manager, Long> {}
